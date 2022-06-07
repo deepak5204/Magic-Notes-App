c421f09cc7d74173dd78e323073d16748eab21a2
deepak <deepakkumar750206@gmail.com>
2022-06-07 18:40:59 +0530

    Add title in the noote card

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 console.log("Magiic notes app");
 showNotes();
+
 //If user adds a note, add it to he localStorae
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', function () {
+    //set title for card
+    let addTitle = document.getElementById('addTitle');
     let addTxt = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -12,28 +15,35 @@ addBtn.addEventListener('click', function () {
         notesObj = JSON.parse(notes);
     }
 
-    notesObj.push(addTxt.value);
+    let myObj ={
+        title: addTitle.value,
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
     showNotes();
 });
 
 //Function to show element from localStorage
 function showNotes() {
     let notes = localStorage.getItem("notes");
+    // console.log(notes);
     if (notes == null) {
         notesObj = [];
     }
     else {
         notesObj = JSON.parse(notes);
     }
-    let html = "";
+    
+    let html = "";// make a blank string
     notesObj.forEach(function (element, index) {
         html += `
             <div class="noteCard my-2 mx-2" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index + 1}</h5>
-                    <p class="card-text"> ${element}</p>
+                    <h5 class="card-title"> ${element.title}</h5>
+                    <p class="card-text"> ${element.text}</p>
                      <button id="${index}" onclick = "deleteNote(this.id)" class="btn btn-primary">Delete</button>
                 </div>
             </div> `
@@ -64,6 +74,8 @@ function deleteNote(index) {
     showNotes();
 }
 
+
+//add search functionality
 let search = document.getElementById("searchTxt");
 search.addEventListener('input', function () {
     let inputVal = search.value.toLowerCase();
@@ -76,6 +88,5 @@ search.addEventListener('input', function () {
         else {
             element.style.display = "none";
         }
-    })
-
-})
+    });
+});
